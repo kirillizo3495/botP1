faaa78366632abbe9f74c9065a7a5812a1922cf4
IT-Cube <izotovkirill54@gmail.com>
2023-04-19 15:46:08 +0300

    я добавил рекваиментс, вооооот

---
+++ botP1.py
@@ -0,0 +1,28 @@
+import requests
+import time
+
+
+API_URL: str = 'https://api.telegram.org/bot'
+BOT_TOKEN: str = '6064408255:AAGBbsdGXRGz70OfVSxpp7mlVOQHxxogsVU'
+TEXT: str = 'что ты мне пишешь?'
+MAX_COUNTER: int = 100             #это количество итераций цикла, в котором мы получаем апдейты от сервера.
+
+offset: int = -2
+counter: int = 0
+chat_id: int
+
+
+while counter < MAX_COUNTER:
+
+    print('attempt =', counter)  #Чтобы видеть в консоли, что код живет
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+
+    time.sleep(1)
+    counter += 1
